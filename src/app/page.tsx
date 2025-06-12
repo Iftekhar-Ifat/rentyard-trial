abd56c5f3602c011b41b86fcdf71d381e551c4e8
Iftekhar-Ifat <iftekharifat007@gmail.com>
2025-06-12 21:10:12 +0600

    added loading and error state from property-selector

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import PropertySelectionForm from "@/components/property/property-selection-form";
+import PropertySelectionForm from "@/components/property-selector/property-selection-form";
 import MaxWidthWrapper from "@/components/shared/max-width-wrapper";
 
 export default function Home() {
---
+++ src/components/property-selector/property-type-selector-error.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button } from "../ui/button";
+
+export default function PropertyTypeSelectorError() {
+  return (
+    <div>
+      <div>Error...</div>
+      <Button>Refetch</Button>
+    </div>
+  );
+}
