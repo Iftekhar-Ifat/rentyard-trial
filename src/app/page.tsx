ff903a1ca85faec40cc01c0443299bf6a9e2d343
Iftekhar-Ifat <iftekharifat007@gmail.com>
2025-06-12 20:59:58 +0600

    added property-type-section & property-type-selector

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
+import PropertySelectionForm from "@/components/property/property-selection-form";
 import MaxWidthWrapper from "@/components/shared/max-width-wrapper";
-import { Button } from "@/components/ui/button";
-import { Notification03Icon } from "@hugeicons/core-free-icons";
-import { HugeiconsIcon } from "@hugeicons/react";
 
 export default function Home() {
   return (
     <MaxWidthWrapper>
-      <div>Hello</div>
-      <Button>Hello</Button>
-      <HugeiconsIcon icon={Notification03Icon} size={24} />
+      <PropertySelectionForm />
     </MaxWidthWrapper>
   );
 }
---
+++ src/components/property/property-selection-form.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useState } from "react";
+import { PropertyTypeSelector } from "./property-type-selector";
+
+export default function PropertySelectionForm() {
+  const [propertyType, setPropertyType] = useState<string>("");
+
+  return (
+    <div className="my-10">
+      <h2 className="text-3xl font-semibold">Property type</h2>
+      <PropertyTypeSelector
+        value={propertyType}
+        onValueChange={setPropertyType}
+      />
+    </div>
+  );
+}
